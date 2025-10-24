7b018ec38c257e907764b52f3685ba04a5b24406
jackg67 <jackglatfelter@gmail.com>
2025-10-24 08:44:00 -0700

    m2q1 done

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 def getPivot(number):
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    average = sum(number) / len(number)
+    pivot = number[0]
+    mindiff = abs(number[0] - average)
+    for i in number:
+        diff = abs(i - average)
+        if diff < mindiff:
+            mindiff = diff
+            pivot = i
+    return pivot
 
 def split(number):
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    sub1 = []
+    sub2 = []
+    pivot = getPivot(number)
+    for i in number:
+        if i <= pivot:
+            sub1.append(i)
+        else:
+            sub2.append(i)
+    
+    return sub1 + [pivot] + sub2
 
 
 def main():
